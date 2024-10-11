9edf83aa0bb4033dd41b343ef9d8f3a9bd11f577
LeeWanVux <vulqpd09254@fpt.edu.vn>
2024-10-11 19:18:00 +0700

    v1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navbar } from "./components";
+import { Home } from "./pages";
+
+const App = () => {
+  return (
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
+
+export default App;
